d5261220d333cc654ffab5e3e534de307688d3e7
Aleksandr <shefera0103@gmail.com>
2023-04-13 21:26:50 +0700

    block1Task6

---
+++ src/main/java/fields/Circle.java
@@ -0,0 +1,21 @@
+package fields;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * this.radius * this.radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
---
+++ src/main/java/fields/Main.java
@@ -0,0 +1,9 @@
+package fields;
+
+public class Main {
+    public static void main(String[] args) {
+        TestCircle test = new TestCircle();
+        test.test();
+    }
+
+}
